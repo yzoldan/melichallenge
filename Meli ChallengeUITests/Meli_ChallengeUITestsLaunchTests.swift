5302a9446efaa6e1c6785bc2a0502e06af90f263
Yoav Zoldan <yzoldan@booz.cl>
2022-07-05 18:17:53 -0400

    Initial Commit

---
+++ Meli ChallengeUITests/Meli_ChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Meli_ChallengeUITestsLaunchTests.swift
+//  Meli ChallengeUITests
+//
+//  Created by Yoav Zoldan on 05-07-2022.
+//
+
+import XCTest
+
+class Meli_ChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
